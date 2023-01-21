15b5f9af2e64bdb6cddd6b9ea9a5444a656e6da7
poojamehta <poojamehta1197@gmail.com>
2023-01-21 22:48:17 +0530

    Add getTokensByTwoParamsTest

--- src/test/java/tests/TokensTest.java
+++ src/test/java/tests/TokensTest.java
@@ -21,7 +21,7 @@ public class TokensTest {
   private static final Logger log = LogManager.getLogger(TokensTest.class.getName());
 
   @DataProvider
-  public Object[][] getTokenTestOneParam() {
+  public Object[][] getTokensByOneParamTest() {
     return new Object[][] {
       {TokenEnum.id},
       {TokenEnum.chainId},
@@ -41,8 +41,28 @@ public class TokensTest {
     };
   }
 
-  @Test(description = "Single parameter", dataProvider = "getTokenTestOneParam")
-  public void getTokenTestOneParam(TokenEnum tokenEnum1) {
+  @DataProvider
+  public Object[][] getTokensByTwoParamsTest() {
+    return new Object[][] {
+      {TokenEnum.id, TokenEnum.chainId},
+      {TokenEnum.id, TokenEnum.name},
+      {TokenEnum.id, TokenEnum.symbol},
+      {TokenEnum.id, TokenEnum.type},
+      {TokenEnum.id, TokenEnum.totalSupply},
+      {TokenEnum.id, TokenEnum.decimals},
+      {TokenEnum.id, TokenEnum.baseURI},
+      {TokenEnum.id, TokenEnum.lastTransferTimestamp},
+      {TokenEnum.id, TokenEnum.lastTransferBlock},
+      {TokenEnum.id, TokenEnum.lastTransferHash},
+      {TokenEnum.id, TokenEnum.currentHolderCount},
+      {TokenEnum.id, TokenEnum.transferCount},
+      {TokenEnum.id, TokenEnum.tokenTraits},
+      {TokenEnum.id, TokenEnum.tokenBalance}
+    };
+  }
+
+  @Test(description = "Single parameter", dataProvider = "getTokensByOneParamTest")
+  public void getTokensByOneParamTest(TokenEnum tokenEnum1) {
     Response res = APIService.sendAPIRequest(TokensHelper.getTokens(tokenEnum1.getValue()));
     log.info("The status code is: " + res.getStatusCode());
     TokenResponse response = res.as(TokenResponse.class);
@@ -53,7 +73,23 @@ public class TokensTest {
       TokensHelper.assertToken(field, tokenEnum1);
       softAssert.assertAll();
     }
+    softAssert.assertAll();
+  }
 
+  @Test(description = "Two parameter", dataProvider = "getTokensByTwoParamsTest")
+  public void getTokensByTwoParamsTest(TokenEnum tokenEnum1, TokenEnum tokenEnum2) {
+    String tokenEnumNew = tokenEnum1.getValue() + tokenEnum2.getValue();
+    Response res = APIService.sendAPIRequest(TokensHelper.getTokens(tokenEnumNew));
+    log.info("The status code is: " + res.getStatusCode());
+    TokenResponse response = res.as(TokenResponse.class);
+    SoftAssert softAssert = new SoftAssert();
+    softAssert.assertNotNull(response.getData());
+    softAssert.assertNotNull(response.data.getTokens());
+    System.out.println("list of tokens" + response.getData().getTokens().getTokens());
+    for (Token field : response.getData().getTokens().getTokens()) {
+      TokensHelper.assertToken(field, tokenEnum1);
+      TokensHelper.assertToken(field, tokenEnum2);
+    }
     softAssert.assertAll();
   }
 }
