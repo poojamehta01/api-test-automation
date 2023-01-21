fcb13efa353ee7d14ea7440547c2668ba01bfb10
poojamehta <poojamehta1197@gmail.com>
2023-01-21 22:34:38 +0530

    Add assertion method in helper

--- src/main/java/helpers/TokensHelper.java
+++ src/main/java/helpers/TokensHelper.java
@@ -1,9 +1,12 @@
 package helpers;
 
+import enums.TokenEnum;
+import org.testng.asserts.SoftAssert;
 import pojo.TokenRequests;
 import pojo.TokenRequests.Filter;
 import pojo.TokenRequests.Input;
 import pojo.TokenRequests.QueryVariables;
+import pojo.TokenResponse.Token;
 
 public class TokensHelper {
 
@@ -33,4 +36,58 @@ public class TokensHelper {
 
     return tokenRequests;
   }
+
+  public static void assertToken(Token field, TokenEnum tokenEnumParam) {
+    SoftAssert softAssert = new SoftAssert();
+    switch (tokenEnumParam) {
+      case id:
+        softAssert.assertNotNull(field.getId());
+        break;
+      case chainId:
+        softAssert.assertNotNull(field.getChainId());
+        break;
+      case name:
+        softAssert.assertNotNull(field.getName());
+        break;
+      case symbol:
+        softAssert.assertNotNull(field.getSymbol());
+        break;
+      case type:
+        softAssert.assertNotNull(field.getType());
+        break;
+      case totalSupply:
+        softAssert.assertNotNull(field.getTotalSupply());
+        break;
+      case decimals:
+        softAssert.assertNotNull(field.getDecimals());
+        break;
+      case baseURI:
+        softAssert.assertNotNull(field.getBaseURI());
+        break;
+      case lastTransferTimestamp:
+        softAssert.assertNotNull(field.getLastTransferTimestamp());
+        String.valueOf(field.getLastTransferTimestamp());
+        break;
+      case lastTransferBlock:
+        softAssert.assertNotNull(field.getLastTransferBlock());
+        break;
+      case lastTransferHash:
+        softAssert.assertNotNull(field.getLastTransferHash());
+        break;
+      case currentHolderCount:
+        softAssert.assertNotNull(field.getCurrentHolderCount());
+        break;
+      case transferCount:
+        softAssert.assertNotNull(field.getTransferCount());
+        break;
+      case tokenTraits:
+        softAssert.assertNull(field.getTokenTraits());
+        break;
+      case tokenBalance:
+        softAssert.assertNull(field.getTokenBalance());
+        break;
+    }
+  }
 }
+
+
--- src/test/java/tests/TokensTest.java
+++ src/test/java/tests/TokensTest.java
@@ -11,6 +11,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pojo.TokenResponse;
+import pojo.TokenResponse.Token;
 import utilities.APIService;
 
 @Epic("Get Token")
@@ -22,25 +23,25 @@ public class TokensTest {
   @DataProvider
   public Object[][] getTokenTestOneParam() {
     return new Object[][] {
-        {TokenEnum.id},
-        {TokenEnum.chainId},
-        {TokenEnum.name},
-        {TokenEnum.symbol},
-        {TokenEnum.type},
-        {TokenEnum.totalSupply},
-        {TokenEnum.decimals},
-        {TokenEnum.baseURI},
-        {TokenEnum.lastTransferTimestamp},
-        {TokenEnum.lastTransferBlock},
-        {TokenEnum.lastTransferHash},
-        {TokenEnum.currentHolderCount},
-        {TokenEnum.transferCount},
-        {TokenEnum.tokenTraits},
-        {TokenEnum.tokenBalance}
+      {TokenEnum.id},
+      {TokenEnum.chainId},
+      {TokenEnum.name},
+      {TokenEnum.symbol},
+      {TokenEnum.type},
+      {TokenEnum.totalSupply},
+      {TokenEnum.decimals},
+      {TokenEnum.baseURI},
+      {TokenEnum.lastTransferTimestamp},
+      {TokenEnum.lastTransferBlock},
+      {TokenEnum.lastTransferHash},
+      {TokenEnum.currentHolderCount},
+      {TokenEnum.transferCount},
+      {TokenEnum.tokenTraits},
+      {TokenEnum.tokenBalance}
     };
   }
-  @Test(
-      description = "Single parameter",dataProvider = "getTokenTestOneParam")
+
+  @Test(description = "Single parameter", dataProvider = "getTokenTestOneParam")
   public void getTokenTestOneParam(TokenEnum tokenEnum1) {
     Response res = APIService.sendAPIRequest(TokensHelper.getTokens(tokenEnum1.getValue()));
     log.info("The status code is: " + res.getStatusCode());
@@ -48,6 +49,11 @@ public class TokensTest {
     SoftAssert softAssert = new SoftAssert();
     softAssert.assertNotNull(response.getData());
     softAssert.assertNotNull(response.data.getTokens());
+    for (Token field : response.getData().getTokens().getTokens()) {
+      TokensHelper.assertToken(field, tokenEnum1);
+      softAssert.assertAll();
+    }
+
     softAssert.assertAll();
   }
 }
