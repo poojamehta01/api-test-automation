15949beadb3e261bb0019f14aa1bf36723792993
poojamehta <poojamehta1197@gmail.com>
2023-01-21 22:24:18 +0530

    Add getTokenTestOneParam testcase with dataprovider

--- src/test/java/tests/TokensTest.java
+++ src/test/java/tests/TokensTest.java
@@ -1,9 +1,53 @@
 package tests;
 
+import enums.TokenEnum;
+import helpers.TokensHelper;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.restassured.response.Response;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pojo.TokenResponse;
+import utilities.APIService;
 
+@Epic("Get Token")
+@Feature("Get Token based on input parameters")
 public class TokensTest {
 
-  @Test(description = "Single parameter")
-  public void getTokenTestOneParam() {}
+  private static final Logger log = LogManager.getLogger(TokensTest.class.getName());
+
+  @DataProvider
+  public Object[][] getTokenTestOneParam() {
+    return new Object[][] {
+        {TokenEnum.id},
+        {TokenEnum.chainId},
+        {TokenEnum.name},
+        {TokenEnum.symbol},
+        {TokenEnum.type},
+        {TokenEnum.totalSupply},
+        {TokenEnum.decimals},
+        {TokenEnum.baseURI},
+        {TokenEnum.lastTransferTimestamp},
+        {TokenEnum.lastTransferBlock},
+        {TokenEnum.lastTransferHash},
+        {TokenEnum.currentHolderCount},
+        {TokenEnum.transferCount},
+        {TokenEnum.tokenTraits},
+        {TokenEnum.tokenBalance}
+    };
+  }
+  @Test(
+      description = "Single parameter",dataProvider = "getTokenTestOneParam")
+  public void getTokenTestOneParam(TokenEnum tokenEnum1) {
+    Response res = APIService.sendAPIRequest(TokensHelper.getTokens(tokenEnum1.getValue()));
+    log.info("The status code is: " + res.getStatusCode());
+    TokenResponse response = res.as(TokenResponse.class);
+    SoftAssert softAssert = new SoftAssert();
+    softAssert.assertNotNull(response.getData());
+    softAssert.assertNotNull(response.data.getTokens());
+    softAssert.assertAll();
+  }
 }
