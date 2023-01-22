0984063a2f6ad27a5ec5b9363384d3b9d3f16250
poojamehta <poojamehta1197@gmail.com>
2023-01-22 23:35:05 +0530

    Add tokenError constants

---
+++ src/main/java/constants/TokenErrorConstants.java
@@ -0,0 +1,12 @@
+package constants;
+
+public class TokenErrorConstants {
+  public static final String ID_ERROR_INCORRECT = "id1";
+
+  public static final String CHAINID_ERROR_INCORRECT = "chainId1";
+
+  public static final String INCORRECT_HTTP_METHOD_ERROR_STATUS_LINE = "HTTP/1.1 404 Not Found";
+  public static final String INCORRECT_CONTENT_TYPE_ERROR_STATUS_LINE = "HTTP/1.1 400 Bad Request";
+
+  public static final String INCORRECT_CONTENT_TYPE_ERROR_MSG = "transport not supported";
+}
