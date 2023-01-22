6e5bb3abb01b0115f313db5dc25acf2e180cb66e
poojamehta <poojamehta1197@gmail.com>
2023-01-22 13:59:38 +0530

    Add response constants

--- src/main/java/constants/TokenResponseConstants.java
+++ src/main/java/constants/TokenResponseConstants.java
@@ -44,8 +44,8 @@ public class TokenResponseConstants {
   public static final String CHAINID_INCORRECT = "chainId1";
   public static final String CHAINID_CORRECT = "chainId";
 
-  public static final String INCORRECT_HTTP_METHOD_ERROR_STATUS_LINE= "HTTP/1.1 404 Not Found";
+  public static final String INCORRECT_HTTP_METHOD_ERROR_STATUS_LINE = "HTTP/1.1 404 Not Found";
   public static final String INCORRECT_CONTENT_TYPE_ERROR_STATUS_LINE = "HTTP/1.1 400 Bad Request";
 
-  public static final String INCORRECT_CONTENT_TYPE_ERROR_MSG ="transport not supported";
+  public static final String INCORRECT_CONTENT_TYPE_ERROR_MSG = "transport not supported";
 }
