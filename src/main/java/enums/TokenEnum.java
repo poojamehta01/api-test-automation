95e6001804ea2e61254f27588a6c903ec7e9a7fe
poojamehta <poojamehta1197@gmail.com>
2023-01-22 13:58:56 +0530

    Change enum name

--- src/main/java/enums/TokenEnum.java
+++ src/main/java/enums/TokenEnum.java
@@ -3,21 +3,21 @@ package enums;
 import lombok.Getter;
 
 public enum TokenEnum {
-  ID("id\n"),
-  CHAIN_ID("chainId\n"),
-  NAME("name\n"),
-  SYMBOL("symbol\n"),
-  TYPE("type\n"),
-  TOTAL_SUPPLY("totalSupply\n"),
-  DECIMALS("decimals\n"),
-  BASE_URI("baseURI\n"),
-  LAST_TRANSFER_TIMESTAMP("lastTransferTimestamp\n"),
-  LAST_TRANSFER_BLOCK("lastTransferBlock\n"),
-  LAST_TRANSFER_HASH("lastTransferHash\n"),
-  CURRENT_HOLDER_COUNT("currentHolderCount\n"),
-  TRANSFER_COUNT("transferCount\n"),
-  TOKEN_TRAITS("tokenTraits\n"),
-  TOKEN_BALANCE("tokenBalance \n {tokenId\nowner\namount\n}");
+  ID_ENUM("id\n"),
+  CHAIN_ID_ENUM("chainId\n"),
+  NAME_ENUM("name\n"),
+  SYMBOL_ENUM("symbol\n"),
+  TYPE_ENUM("type\n"),
+  TOTAL_SUPPLY_ENUM("totalSupply\n"),
+  DECIMALS_ENUM("decimals\n"),
+  BASE_URI_ENUM("baseURI\n"),
+  LAST_TRANSFER_TIMESTAMP_ENUM("lastTransferTimestamp\n"),
+  LAST_TRANSFER_BLOCK_ENUM("lastTransferBlock\n"),
+  LAST_TRANSFER_HASH_ENUM("lastTransferHash\n"),
+  CURRENT_HOLDER_COUNT_ENUM("currentHolderCount\n"),
+  TRANSFER_COUNT_ENUM("transferCount\n"),
+  TOKEN_TRAITS_ENUM("tokenTraits\n"),
+  TOKEN_BALANCE_ENUM("tokenBalance \n {tokenId\nowner\namount\n}");
 
   @Getter private final String value;
 
