764838b4d8b2a7384ea9d50d9f5f87ca3e4e8925
poojamehta <poojamehta1197@gmail.com>
2023-01-22 23:29:54 +0530

    Change className

--- src/main/java/helpers/TokenQueryParamNegativeDataHelper.java
+++ src/main/java/helpers/TokenQueryParamNegativeDataHelper.java
@@ -1,7 +1,7 @@
 package helpers;
 
-import static constants.TokenResponseConstants.CHAINID_INCORRECT;
-import static constants.TokenResponseConstants.ID_INCORRECT;
+import static constants.TokenErrorConstants.CHAINID_ERROR_INCORRECT;
+import static constants.TokenErrorConstants.ID_ERROR_INCORRECT;
 import static enums.TokenErrorResponseEnums.GET_TOKEN_QUERY_INVALID_CHAINID_FIELD_NAME;
 import static enums.TokenErrorResponseEnums.GET_TOKEN_QUERY_INVALID_ID_FIELD_NAME;
 import static enums.TokenErrorResponseEnums.GET_TOKEN_QUERY_INVALID_NOT_PRESENT_FIELD_INT;
@@ -12,15 +12,15 @@ import io.restassured.http.ContentType;
 import java.lang.reflect.Method;
 import org.testng.annotations.DataProvider;
 
-public class TokenNegativeDataHelper {
+public class TokenQueryParamNegativeDataHelper {
   @DataProvider(name = "token_dataProvider")
   public static Object[][] token_dataProvider(Method m) {
 
     switch (m.getName()) {
       case "getTokensByIncorrectParamTest":
         return new Object[][] {
-          {ID_INCORRECT, GET_TOKEN_QUERY_INVALID_ID_FIELD_NAME},
-          {CHAINID_INCORRECT, GET_TOKEN_QUERY_INVALID_CHAINID_FIELD_NAME},
+          {ID_ERROR_INCORRECT, GET_TOKEN_QUERY_INVALID_ID_FIELD_NAME},
+          {CHAINID_ERROR_INCORRECT, GET_TOKEN_QUERY_INVALID_CHAINID_FIELD_NAME},
           {"test", GET_TOKEN_QUERY_INVALID_NOT_PRESENT_FIELD_NAME},
           {"123", GET_TOKEN_QUERY_INVALID_NOT_PRESENT_FIELD_INT},
           {"\"id\"", GET_TOKEN_QUERY_INVALID_NOT_PRESENT_FIELD_STRING}
