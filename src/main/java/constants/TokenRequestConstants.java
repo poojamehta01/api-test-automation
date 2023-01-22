a6a29314fda9c5f32ffa617fd9351f6a5f480f49
poojamehta <poojamehta1197@gmail.com>
2023-01-22 23:25:11 +0530

    Add token request constants

---
+++ src/main/java/constants/TokenRequestConstants.java
@@ -0,0 +1,18 @@
+package constants;
+
+public class TokenRequestConstants {
+
+  public static final String baseURI = "https://devapi.airstack.xyz/gql";
+
+  public static final String CHAIN_ID_DEFAULT = "1";
+  public static final String TYPE_DEFAULT = "";
+  public static final String NAME_DEFAULT = "";
+
+  public static final String SYMBOL_DEFAULT = "";
+
+  public static final String CURSOR_DEFAULT = "";
+  public static final int LIMIT_DEFAULT = 1;
+
+  // failing for this address - 00000000000045166c45af0fc6e4cf31d9e14b9a
+  public static final String ADDRESS_DEFAULT = "0x60e4d786628fea6478f785a6d7e704777c86a7c6";
+}
